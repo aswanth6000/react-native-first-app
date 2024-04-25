477e7acb4f580a2288e6361056e7707519710791
aswanth6000 <aswanth682@gmail.com>
2024-04-25 20:30:36 +0530

    Modal

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Pressable,
+  Modal,
+} from "react-native";
+import { useState } from "react";
 const logoImage = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModal, setIsModal] = useState(false);
+  function handleOnclick() {
+    setIsModal(!isModal);
+  }
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title="Click me " onPress={()=> console.log("button presed")} color="midnightblue" disabled/>
+      <Button
+        title="Click me "
+        onPress={() => setIsModal(true)}
+        color="midnightblue"
+      />
+      <Modal visible={isModal} onRequestClose={()=> setIsModal(false)} animationType="slide" presentationStyle="formSheet">
+        <Text>Modal Content</Text>
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Button
+            title="Click me "
+            onPress={() => setIsModal(false)}
+            color="midnightblue"
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
