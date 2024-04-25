77f6986697bc5bf8ff03e442a8ae6d4eb3ca20e8
aswanth6000 <aswanth682@gmail.com>
2024-04-25 20:33:08 +0530

    status bar

--- App.js
+++ App.js
@@ -7,32 +7,15 @@ import {
   Button,
   Pressable,
   Modal,
+  StatusBar
 } from "react-native";
 import { useState } from "react";
 const logoImage = require("./assets/adaptive-icon.png");
 
 export default function App() {
-  const [isModal, setIsModal] = useState(false);
-  function handleOnclick() {
-    setIsModal(!isModal);
-  }
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="Click me "
-        onPress={() => setIsModal(true)}
-        color="midnightblue"
-      />
-      <Modal visible={isModal} onRequestClose={()=> setIsModal(false)} animationType="slide" presentationStyle="formSheet">
-        <Text>Modal Content</Text>
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Button
-            title="Click me "
-            onPress={() => setIsModal(false)}
-            color="midnightblue"
-          />
-        </View>
-      </Modal>
+      <StatusBar backgroundColor="lightgreen" barStyle={"dark-content"} hidden/>
     </View>
   );
 }
