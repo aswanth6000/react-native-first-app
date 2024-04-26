6c79556ce6c8382ab39f2f621b65ee00d88dab32
aswanth6000 <aswanth682@gmail.com>
2024-04-26 13:34:33 +0530

    styling

--- App.js
+++ App.js
@@ -3,13 +3,32 @@ import {View, Text, StyleSheet} from 'react-native'
 export default function App(){
   return (
     <View style={styles.container}>
+    <View style={styles.lightGreenBox}>
       <Text>
-      StyleSheet API
+      Light blue
       </Text>
+    </View>
+    <View style={styles.lightBlueBox}>
+      <Text>
+      leght blue
+      </Text>
+    </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor:"lightgreen", padding: 60}
+  container: {flex: 1, backgroundColor:"lightyellow", padding: 60},
+  lightBlueBox: {
+    backgroundColor:"lightblue",
+    width:100,
+    height:100,
+    padding:10
+  },
+  lightGreenBox: {
+    backgroundColor:"lightgreen",
+    width:100,
+    height:100,
+    padding:10
+  },
 })
