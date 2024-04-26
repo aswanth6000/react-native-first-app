3a8b5c1e704ff43bd29ab8ba432b31083d1ffdf5
aswanth6000 <aswanth682@gmail.com>
2024-04-26 12:01:31 +0530

    style sheet api

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
-import {
-  View,
-  Text,
-  Image,
-  ImageBackground,
-  ScrollView,
-  Button,
-  Pressable,
-  Modal,
-  StatusBar,
-  ActivityIndicator,
-  Alert
-} from "react-native";
-import Greet from "./components/Greet";
-import { useState } from "react";
-const logoImage = require("./assets/adaptive-icon.png");
+import {View, Text, StyleSheet} from 'react-native'
 
-export default function App() {
+export default function App(){
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Greet name={"kona"}/>
+    <View style={styles.container}>
+      <Text>
+      StyleSheet API
+      </Text>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor:"lightgreen", padding: 60}
+})
